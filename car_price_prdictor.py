4b56501bb558d9cd5a20fbb1ca9ca872674cd5c5
thatikonda ramakrishna <krishthatikonda1317@gmail.com>
2024-07-26 20:37:19 +0545

    car_price_predictor

--- car_price_prdictor.py
+++ car_price_prdictor.py
@@ -1,5 +1,6 @@
 import pandas as pd
 import numpy as np
+import sklearn
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import r2_score,accuracy_score
 from sklearn.compose import ColumnTransformer,make_column_transformer
